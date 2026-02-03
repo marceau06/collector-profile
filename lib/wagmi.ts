c94311b22653085444576eff67bb098cedace03e
marceau <marceaule06@gmail.com>
2026-02-03 23:04:58 +0100

    Add rainbowkit provider

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,9 +1,12 @@
+'use client'
+
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { sepolia } from 'wagmi/chains';
+import { sepolia } from 'viem/chains';
+import { abstract } from 'viem/chains';
 
 export const config = getDefaultConfig({
   appName: 'Collector Profile',
   projectId: 'VOTRE_PROJECT_ID',
-  chains: [sepolia],
+  chains: [sepolia, abstract],
   ssr: true,
 });
